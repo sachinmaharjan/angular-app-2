58f4ffe6642b3816509b9f2c56d7a657835d675d
Sachin Maharjan <sachin.gen@gmail.com>
2015-04-14 15:22:54 -0700

    Angular app 2

---
+++ app/maincontroller.js
@@ -0,0 +1,49 @@
+app.controller("MainController", function($scope){
+	$scope.selectedPerson = 0;
+	$scope.selectedGenre = null;
+	$scope.people = [
+		{
+			id: 0,
+			name: 'Leon',
+			music: [
+				'Rock',
+				'Metal',
+				'Dubstep',
+				'Electro'
+			],
+			live: true
+		},
+		{
+			id: 1,
+			name: 'Chris',
+			music: [
+				'Indie',
+				'Drumstep',
+				'Dubstep',
+				'Electro'
+			],
+			live: false
+		},
+		{
+			id: 2,
+			name: 'Harry',
+			music: [
+				'Rock',
+				'Metal',
+				'Thrash Metal',
+				'Heavy Metal'
+			],
+			live: true
+		},
+		{
+			id: 3,
+			name: 'Allyce',
+			music: [
+				'Pop',
+				'RnB',
+				'Hip Hop'
+			],
+			live: true
+		}
+	];
+});
---
+++ app/readme.txt
@@ -0,0 +1,2 @@
+http://www.revillweb.com/tutorials/angularjs-in-30-minutes-angularjs-tutorial/
+
